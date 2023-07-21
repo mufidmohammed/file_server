518edf5df5b7c41aba0e57cce7bc260a91ef14be
mufidmohammed <mufidmohammed200@gmail.com>
2023-07-21 12:18:21 +0000

    added feed page

--- server/views.py
+++ server/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from .models import File
+
+
 # Create your views here.
+def index(request):
+    files = File.objects.all()
+    return render(request, "server/feed.html", {"files": files})
