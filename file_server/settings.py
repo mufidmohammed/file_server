68a0b1d903478ffe51e911b902a0c6551af9e42c
mufidmohammed <mufidmohammed200@gmail.com>
2023-07-21 05:44:30 +0000

    settings for media uploads

--- file_server/settings.py
+++ file_server/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -134,3 +134,6 @@ JAZZMIN_SETTINGS = {
     'welcome_sign': 'Admin Login',
     'hide_apps': ['auth'],
 }
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
