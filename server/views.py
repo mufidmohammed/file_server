c938beea33b44eaaf552907082093419c8f3f3db
mufidmohammed <mufidmohammed200@gmail.com>
2023-07-21 12:23:10 +0000

    implement feed search

--- server/views.py
+++ server/views.py
@@ -5,5 +5,9 @@ from .models import File
 
 # Create your views here.
 def index(request):
-    files = File.objects.all()
+    if request.method == "POST":
+        search = request.POST["search"]
+        files = File.objects.filter(title__contains=search)
+    else:
+        files = File.objects.all()
     return render(request, "server/feed.html", {"files": files})
