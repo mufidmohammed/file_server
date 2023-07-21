cc40f3d3732e471980a3b431a602a1162c0ee728
mufidmohammed <mufidmohammed200@gmail.com>
2023-07-21 05:40:10 +0000

    set up admin to upload and view files

--- server/admin.py
+++ server/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import File
+
+
 # Register your models here.
+class FileAdmin(admin.ModelAdmin):
+    fields = [
+        "title", "description", "file"
+    ]
+
+    list_display = ["title", "previews", "emails_sent"]
+
+
+admin.site.register(File, FileAdmin)
--- server/models.py
+++ server/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class File(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    file = models.FileField()
+    previews = models.IntegerField(default=0)
+    emails_sent = models.IntegerField(default=0)
