8d62a14d83fd0eb48bd39039791bcc5d0d2e8799
mufidmohammed <mufidmohammed200@gmail.com>
2023-07-22 08:37:40 +0000

    development setup

--- file_server/settings.py
+++ file_server/settings.py
@@ -138,13 +138,8 @@ JAZZMIN_SETTINGS = {
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp-relay.sendinblue.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mufidmohammed200@gmail.com'
-EMAIL_HOST_PASSWORD = 'OcPwM8q7p3ImkYxR'
-
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
 
 LOGIN_REDIRECT_URL = '/admin'
 LOGIN_URL = 'login/'
